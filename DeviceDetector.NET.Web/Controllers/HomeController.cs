7a3bdcc42c72c42bb509d9f093934edaa0c4a6f5
totpero <www.totpe.ro@gmail.com>
2023-03-05 18:54:02 +0200

    .

--- DeviceDetector.NET.Web/Controllers/HomeController.cs
+++ DeviceDetector.NET.Web/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using DeviceDetectorNET.Parser;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Linq;
 
 namespace DeviceDetectorNET.Web.Controllers
 {
@@ -10,13 +9,13 @@ namespace DeviceDetectorNET.Web.Controllers
     {
         public IActionResult Index()
         {
-            DeviceDetectorNET.DeviceDetector.SetVersionTruncation(VersionTruncation.VERSION_TRUNCATION_NONE);
+            DeviceDetector.SetVersionTruncation(VersionTruncation.VERSION_TRUNCATION_NONE);
 
             var userAgent = Request.Headers["User-Agent"];
 
             //var headers = Request.Headers.ToDictionary(a => a.Key, a => a.Value);
 
-            var result = DeviceDetectorNET.DeviceDetector.GetInfoFromUserAgent(userAgent);
+            var result = DeviceDetector.GetInfoFromUserAgent(userAgent);
 
             var output = result.Success ? result.ToString().Replace(Environment.NewLine, "<br />") : "Unknown";
 
