346104d2c24c0c12e1d6b3a33e3b6b3320416216
totpero <www.totpe.ro@gmail.com>
2025-11-14 12:08:54 +0200

    #99 start project

---
+++ Serilog.Enrichers.AspNetCore.DeviceDetector/DeviceDetectorConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog.Configuration;
+using Serilog.Enrichers;
+using System;
+
+namespace Serilog
+{
+    public static class DeviceDetectorConfigurationExtensions
+    {
+        /// <summary>
+        /// Enrich log events with Aspnetcore httpContext properties.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <param name="serviceProvider"></param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration WithDeviceDetector(this LoggerEnrichmentConfiguration enrichmentConfiguration,
+            IServiceProvider serviceProvider)
+        {
+            ArgumentNullException.ThrowIfNull(enrichmentConfiguration);
+
+            return enrichmentConfiguration.With<DeviceDetectorEnricher>();
+        }
+    }
+}
---
+++ Serilog.Enrichers.AspNetCore.DeviceDetector/Enrichers/DeviceDetectorEnricher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using DeviceDetectorNET;
+using DeviceDetectorNET.Parser;
+using Microsoft.AspNetCore.Http;
+using Serilog.Core;
+using Serilog.Events;
+using System.Linq;
+
+namespace Serilog.Enrichers;
+
+public class DeviceDetectorEnricher(IHttpContextAccessor httpContextAccessor) : ILogEventEnricher
+{
+    private const string DeviceDetectorPropertyName = "DeviceDetector";
+    private const string DeviceDetectorItemKey = "Serilog_DeviceDetector";
+
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="DeviceDetectorEnricher" /> class.
+    /// </summary>
+    public DeviceDetectorEnricher() : this(new HttpContextAccessor())
+    {
+    }
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        HttpContext httpContext = httpContextAccessor!.HttpContext;
+        if (httpContext == null) return;
+
+        //if (httpContext.Items.TryGetValue(DeviceDetectorItemKey, out object value) &&
+        //    value is LogEventProperty logEventProperty)
+        //{
+
+        //}
+        //else
+        //{
+
+        //}
+
+        if (httpContext.Request!.Headers!.TryGetValue("User-Agent", out var userAgent))
+        {
+            DeviceDetector.SetVersionTruncation(VersionTruncation.VERSION_TRUNCATION_NONE);
+            var headers = httpContext.Request!.Headers.ToDictionary(a => a.Key, a => a.Value.ToArray().FirstOrDefault());
+            var clientHints = ClientHints.Factory(headers);  // client hints are optional
+            var result = DeviceDetector.GetInfoFromUserAgent(userAgent, clientHints);
+
+            var deviceDetectorProperty = propertyFactory.CreateProperty(DeviceDetectorPropertyName, result, destructureObjects: true);
+            httpContext.Items.TryAdd(DeviceDetectorItemKey, deviceDetectorProperty);
+            logEvent.AddPropertyIfAbsent(deviceDetectorProperty);
+        }
+    }
+}
